80755351d11292cefba7245a27613ff941499ac4
Rylan <ryevtrom@gmail.com>
2023-11-05 18:21:10 -0500

    Updated Spawn Rate
    Fixed the sloppy code in the enemy behavior and then gave a spawn command for enemy three.

--- Assets/Scipts/GameManager.cs
+++ Assets/Scipts/GameManager.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject enemyOnePrefab;
+    public GameObject enemyThreePrefab;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("CreateEnemyOne", 1.0f, 3.0f);
+        InvokeRepeating("CreateEnemyThree", 5.0f, 8.0f);
     }
 
     // Update is called once per frame
@@ -21,5 +23,11 @@ public class GameManager : MonoBehaviour
     void CreateEnemyOne()
     {
         Instantiate(enemyOnePrefab, new Vector3(Random.Range(-8, 8), 7, 0), Quaternion.identity);
+        
+    }
+    void CreateEnemyThree()
+    {
+        Instantiate(enemyThreePrefab, new Vector3(Random.Range(-8, 15), 7, 0), Quaternion.identity);
+
     }
 }
